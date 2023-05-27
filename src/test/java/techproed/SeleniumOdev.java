4ebb6c39c3024444efdd2f2c197e0c7cc0dab877
kivanc <kivanckivanc5892@gmail.com>
2023-05-27 23:37:37 +0300

    27/05/2023

---
+++ src/test/java/techproed/SeleniumOdev.java
@@ -0,0 +1,45 @@
+package techproed;
+
+public class SeleniumOdev {
+    public static void main(String[] args) {
+
+        //Yeni bir Class olusturalim.C07_ManageWindowSet
+        //Amazon soyfasina gidelim. https://www.amazon.com/
+        //Sayfanin konumunu ve boyutlarini yazdirin
+        //Sayfanin konumunu ve boyutunu istediginiz sekilde ayarlayin
+        //Sayfanin sizin istediginiz konum ve boyuta geldigini test edin
+        //8. Sayfayi kapatin
+
+
+       // Yeni bir class olusturalim (Homework)
+        //ChromeDriver kullanarak, facebook sayfasina gidin ve sayfa basliginin (title) “facebook”
+        //oldugunu dogrulayin (verify), degilse dogru basligi yazdirin.
+        //        Sayfa URL’inin “facebook” kelimesi icerdigini dogrulayin, icermiyorsa “actual” URL’i
+        //yazdirin.
+         //       https://www.walmart.com/ sayfasina gidin.
+        //Sayfa basliginin “Walmart.com” icerdigini dogrulayin.
+        //Tekrar “facebook” sayfasina donun
+        //Sayfayi yenileyin
+        //Sayfayi tam sayfa (maximize) yapin  9.Browser’i kapatin
+
+
+        /*
+        Yeni bir class olusturun (TekrarTesti)
+Youtube web sayfasına gidin ve sayfa başlığının “youtube” olup olmadığını doğrulayın  (verify), eğer değilse doğru başlığı(Actual Title) konsolda yazdirin.
+Sayfa URL'sinin “youtube” içerip içermediğini (contains) doğrulayın, içermiyorsa doğru  URL'yi yazdırın.
+Daha sonra Amazon sayfasina gidin https://www.amazon.com/
+Youtube sayfasina geri donun
+Sayfayi yenileyin
+Amazon sayfasina donun
+Sayfayi tamsayfa yapin
+Ardından sayfa başlığının "Amazon" içerip içermediğini (contains) doğrulayın, Yoksa  doğru başlığı(Actual Title) yazdırın.
+Sayfa URL'sinin https://www.amazon.com/ olup olmadığını doğrulayın, degilse doğru
+URL'yi yazdırın
+Sayfayi kapatin
+
+
+         */
+
+
+    }
+}
